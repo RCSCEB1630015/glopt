bda41761e84090fff720698f6d61d5ef25edee37
A <M>
2020-01-17 21:39:00 +0300

    add carousel

---
+++ dist/js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+      centerMode: true,
+      centerPadding: '40px',
+      slidesToShow: 3,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 3
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 1
+          }
+        }
+      ]
+    });
+  });
--- gulpfile.js
+++ gulpfile.js
@@ -32,6 +32,7 @@ gulp.task('styles', function() {
 gulp.task('watch', function() {
     gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'));
     gulp.watch("src/*.html").on('change', gulp.parallel('html'));
+    gulp.watch("src/js/**/*.js").on('change', gulp.parallel('scripts')); // если шо удалишь
 });
 
 gulp.task('html', function () {
@@ -42,7 +43,8 @@ gulp.task('html', function () {
 
 gulp.task('scripts', function () {
     return gulp.src("src/js/**/*.js")
-        .pipe(gulp.dest("dist/js"));
+        .pipe(gulp.dest("dist/js"))
+        .on("end", browserSync.reload);
 });
 
 gulp.task('fonts', function () {
