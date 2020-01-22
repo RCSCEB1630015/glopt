7c769790b3759fde562bccfc953a70271db77029
A <M>
2020-01-22 22:58:52 +0300

    3.2

--- src/js/script.js
+++ src/js/script.js
@@ -1,27 +1,24 @@
 $(document).ready(function(){
-    $('.carousel__inner').slick({
-      centerMode: true,
-      centerPadding: '40px',
-      slidesToShow: 3,
-      responsive: [
-        {
-          breakpoint: 768,
-          settings: {
-            arrows: false,
-            centerMode: true,
-            centerPadding: '40px',
-            slidesToShow: 3
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            arrows: false,
-            centerMode: true,
-            centerPadding: '40px',
-            slidesToShow: 1
-          }
-        }
-      ]
-    });
+  $('.carousel__photo').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode: true,
+    /* asNavFor: '.carousel__descr', */
+    prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrow_left.svg"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="../icons/arrow_right.svg"></button>',
   });
+
+  /* $('.carousel__descr').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    asNavFor : '.carousel__photo',
+    centerMode: true,
+  }); */
+
+
+
+
+
+});
+
